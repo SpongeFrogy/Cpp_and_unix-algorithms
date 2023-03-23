72a4e38b9f128a8e97e314d92dfc0d35bb5cb6ec
SpongeFrogy <droidb1poc@gmail.com>
2023-03-23 12:51:08 +0300

    done CLAC

--- lab3/CLAC.cpp
+++ lab3/CLAC.cpp
@@ -5,26 +5,38 @@
 #include <stdio.h>
 #include <string.h>
 
-
 using namespace std;
 
 int main()
 {
-    char input;
-    std::cout << "Write an operation:";
-    std::cin >> input;
-    std::string operand[4] = 
-    {"calc", "plus", "minus", "power"};
-    for(int i = 0; i<4; i++)
+    std::string s;
+    std::cout<<"example:1.23plus4.46\ncommands:plus / minus / power\n";
+    std::cout<<"Write a command:";
+    std::cin>>s;
+    //size_t pos = s.find("plus");
+    if (s.find("plus") != std::string::npos)
     {
-        char *token = strtok(input, operand[i]);
-   
-        // Keep printing tokens while one of the
-        // delimiters present in str[].
-        while (token != NULL)
+        size_t pos = s.find("plus");
+        double x1 = std::stod(s.substr(0, pos));
+        double x2 = std::stod(s.substr(pos+4));
+        std::cout<<"="<<x1+x2;
+        return 0;
+    }
+    if (s.find("minus") != std::string::npos)
     {
-        printf("%s\n", token);
-        token = strtok(NULL, "-");
+        size_t pos = s.find("minus");
+        double x1 = std::stod(s.substr(0, pos));
+        double x2 = std::stod(s.substr(pos+5));
+        std::cout<<"="<<x1-x2;
+        return 0;
     }
+    if (s.find("power") != std::string::npos)
+    {
+        size_t pos = s.find("power");
+        double x1 = std::stod(s.substr(0, pos));
+        double x2 = std::stod(s.substr(pos+5));
+        std::cout<<"="<<pow(x1, x2);
+        return 0;
     }
+    return 0;
 }
