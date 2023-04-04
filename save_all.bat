26f14aef05ba93e73b2214aadb80afe64fbd427f
SpongeFrogy <droidb1poc@gmail.com>
2023-04-04 23:15:38 +0300

    script_commit

--- save_all.bat
+++ save_all.bat
@@ -1,43 +1,34 @@
 cd C:\Users\droid\Projects\6sem_Cpp_and_unix
-echo The echoed text
 @pause
 git switch main
-echo The echoed text
 @pause
 git add -A
-echo The echoed text
 @pause
 git commit -m script_commit
-echo The echoed text
 @pause
 git push
-echo The echoed text
 @pause
 git switch dev
-echo The echoed text
+git reset
 @pause
 git merge --commit main
-echo The echoed text
 @pause
 git add -A
-echo The echoed text
 @pause
 git commit -m script_commit
-echo The echoed text
 @pause
 git push 
 git switch stg
-echo The echoed text
+git reset
 @pause
 git merge --commit dev
-echo The echoed text
 @pause
 git add -A
 git commit -m script_commit
-echo The echoed text
 @pause
 git push   
 git switch prd
+git reset
 git merge --commit stg
 git add -A
 git commit -m script_commit
