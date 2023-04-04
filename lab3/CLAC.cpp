aaed39b140e8439b8e38df485b6084adc4f06e72
SpongeFrogy <droidb1poc@gmail.com>
2023-04-04 23:45:46 +0300

    re

--- lab3/CLAC.cpp
+++ lab3/CLAC.cpp
@@ -7,37 +7,6 @@
 
 using namespace std;
 
-
-int main()
-{
-    std::string s;
-    std::cout<<"example:1.23plus4.46\ncommands:plus / minus / power\n";
-    std::cout<<"Write a command:";
-    std::cin>>s;
-    //size_t pos = s.find("plus");
-    if (s.find("plus") != std::string::npos)
-    {
-        size_t pos = s.find("plus");
-        double x1 = std::stod(s.substr(0, pos));
-        double x2 = std::stod(s.substr(pos+4));
-        std::cout<<"="<<x1+x2;
-        return 0;
-    }
-    if (s.find("minus") != std::string::npos)
-    {
-        size_t pos = s.find("minus");
-        double x1 = std::stod(s.substr(0, pos));
-        double x2 = std::stod(s.substr(pos+5));
-        std::cout<<"="<<x1-x2;
-        return 0;
-    }
-    if (s.find("power") != std::string::npos)
-    {
-        size_t pos = s.find("power");
-        double x1 = std::stod(s.substr(0, pos));
-        double x2 = std::stod(s.substr(pos+5));
-        std::cout<<"="<<pow(x1, x2);
-
 //https://www.geeksforgeeks.org/command-line-arguments-in-c-cpp/
 int main(int argc, char* argv[])
 {
--- lab3/HanoiTower.cpp
+++ lab3/HanoiTower.cpp
@@ -8,8 +8,6 @@ template<typename T>
 void printList(std::list<T> l) {
     for (const auto &item : l) {
         cout << item << "; " << endl;
-        cout << item << "; ";
-        cout << item << "; " << endl;
     }
     cout << endl;
 }
@@ -23,41 +21,22 @@ std::list<string> towerOfHanoi(int n, char from_rod, char to_rod,
     list = towerOfHanoi(n - 1, from_rod, aux_rod, to_rod, list);
     //cout << "Move disk " << n << " from rod " << from_rod
     //     << " to rod " << to_rod << endl;
-{   cout<< n<< "f"<< from_rod<< to_rod << endl;
-    if (n == 0) {
-        return list;
-    }
-    cout<< n<< "s"<< from_rod<< to_rod << endl;
-    list = towerOfHanoi(n - 1, from_rod, aux_rod, to_rod, list);
-    //cout << "Move disk " << n << " from rod " << from_rod
-    //     << " to rod " << to_rod << endl;
-    cout<<n<<"th"<< from_rod<< to_rod << endl;
     string s = to_string(n).append(":");
     s+=from_rod;
     s.append("->");
     s+=to_rod;
     list.push_back(s);
     list = towerOfHanoi(n - 1, aux_rod, to_rod, from_rod, list);
-    cout<<n<<"fo"<< from_rod<< to_rod << endl;
-
-    cout<<n<<"fo"<< from_rod<< to_rod << endl;
-
     return list;
 }
   
 int main()
 {
-    int N = 2;
-
     int N = 8;
-
-    int N = 2;
-
     std::list<string> l = {};
     // A, B and C are names of rods
     l = towerOfHanoi(N, 'A', 'C', 'B', l);
     printList(l);
-    l.clear(); //удаляем из памяти
     l.clear(); //удаляем из памяти
     return 0;
 }
