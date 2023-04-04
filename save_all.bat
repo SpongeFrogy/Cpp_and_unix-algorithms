54f2f4ba69bd884f6215cd9a3a9a0cc415a938f3
SpongeFrogy <droidb1poc@gmail.com>
2023-04-04 23:09:53 +0300

    script_commit

--- save_all.bat
+++ save_all.bat
@@ -5,16 +5,26 @@ git switch main
 echo The echoed text
 @pause
 git add -A
+echo The echoed text
+@pause
 git commit -m script_commit
 echo The echoed text
 @pause
 git push
+echo The echoed text
+@pause
 git switch dev
 echo The echoed text
 @pause
 git merge --commit main
+echo The echoed text
+@pause
 git add -A
+echo The echoed text
+@pause
 git commit -m script_commit
+echo The echoed text
+@pause
 git push 
 git switch stg
 echo The echoed text
