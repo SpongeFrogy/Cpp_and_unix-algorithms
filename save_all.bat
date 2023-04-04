f4420b787088f878e4a0b0272c903770e7d0cb6a
SpongeFrogy <droidb1poc@gmail.com>
2023-04-04 23:23:55 +0300

    script_commit

--- save_all.bat
+++ save_all.bat
@@ -1,29 +1,22 @@
 cd C:\Users\droid\Projects\6sem_Cpp_and_unix
-@pause
 git switch main
-@pause
 git add -A
-@pause
 git commit -m script_commit
-@pause
 git push
-@pause
 git switch dev
 git reset
-@pause
 git merge --commit main
-@pause
 git add -A
-@pause
 git commit -m script_commit
-@pause
 git push 
 git switch stg
+@pause
 git reset
 @pause
 git merge --commit dev
 @pause
 git add -A
+@pause
 git commit -m script_commit
 @pause
 git push   
